47aac52a5e56ae6e758a690cedf7f0b744511736
kevinDronnn <voroshilin777@icloud.com>
2024-05-08 16:46:09 +0300

    Initial commit

---
+++ src/main/java/com/example/codereviewnovaserve/CodeReviewNovaServeApplication.java
@@ -0,0 +1,13 @@
+package com.example.codereviewnovaserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeReviewNovaServeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeReviewNovaServeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/codereviewnovaserve/CodeReviewNovaServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codereviewnovaserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeReviewNovaServeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
